d6910e6b3edfd5bd10b55ba5f4b7ed23c4ded72f
Corbio <113342618+Corbiobio@users.noreply.github.com>
2025-06-19 10:56:41 +0200

    put \0 to message

--- src/Handler_receive.cpp
+++ src/Handler_receive.cpp
@@ -3,10 +3,10 @@
 /*                                                        :::      ::::::::   */
 /*   Handler_receive.cpp                                :+:      :+:    :+:   */
 /*                                                    +:+ +:+         +:+     */
-/*   By: sflechel <sflechel@student.42lyon.fr>      +#+  +:+       +#+        */
+/*   By: edarnand <edarnand@student.42lyon.fr>      +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2025/06/17 16:56:49 by sflechel          #+#    #+#             */
-/*   Updated: 2025/06/18 09:17:59 by sflechel         ###   ########.fr       */
+/*   Updated: 2025/06/19 10:56:06 by edarnand         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -16,13 +16,15 @@
 
 void	Handler_receive::read_data_sent()
 {
-	char	read_buffer[READ_BUFFER_SIZE];
+	char	read_buffer[READ_BUFFER_SIZE + 1];
 	int		bytes_read = READ_BUFFER_SIZE;
 
 	std::cout << "Message received!" << std::endl;
+	read_buffer[0] = 0;
 	while (bytes_read == READ_BUFFER_SIZE)
 	{
 		bytes_read = recv(this->m_sock_fd, read_buffer, READ_BUFFER_SIZE, 0);
+		read_buffer[bytes_read] = 0;
 		std::string message(read_buffer);
 		std::cout << message;
 	}
