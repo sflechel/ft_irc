e1712be904b1df870fc216a308c055cbcb71ae8b
Corbio <113342618+Corbiobio@users.noreply.github.com>
2025-06-20 11:32:07 +0200

    improve parsing func

--- src/Handler_receive.cpp
+++ src/Handler_receive.cpp
@@ -6,7 +6,7 @@
 /*   By: edarnand <edarnand@student.42lyon.fr>      +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2025/06/17 16:56:49 by sflechel          #+#    #+#             */
-/*   Updated: 2025/06/20 10:53:17 by edarnand         ###   ########.fr       */
+/*   Updated: 2025/06/20 11:31:52 by edarnand         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -20,9 +20,9 @@ void	Handler_receive::parse_client_msg(char *msg, int msg_len)
 {
 	if (msg[0] == '\0')
 		return ;
-	char *crlf = std::find(msg, msg + msg_len, "\r\n");
-	if (!crlf)
-		return ;//ignore empty msg
+	char *crlf = std::find(msg, msg + msg_len, '\r');
+	if (!crlf || *(crlf + 1) != '\n')
+		return ;//ignore empty and invalid msg
 
 	char *first_space = std::find(msg, msg + msg_len,  ' ');
 	char *cmd = msg;
@@ -35,7 +35,8 @@ void	Handler_receive::parse_client_msg(char *msg, int msg_len)
 	}
 	else
 		params = NULL;
-	std::cout << cmd << " " << params << std::endl;
+
+	std::cout << cmd << " | " << params;
 }
 
 void	Handler_receive::read_data_sent()
@@ -53,13 +54,13 @@ void	Handler_receive::read_data_sent()
 		std::string message(read_buffer);
 		std::cout << message;
 	}
-	std::cout << std::endl;
 
 	parse_client_msg(read_buffer, bytes_read);
 }
 
 Handler_receive::Handler_receive(Client client) : m_client(client)
 {
+	(void)m_sock_fd;
 }
 
 Handler_receive::~Handler_receive()
